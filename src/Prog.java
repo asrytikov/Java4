a223c3985b0b65663dfda7a444cfb357b8253baf
asrytikov <44636190+asrytikov@users.noreply.github.com>
2021-11-04 10:54:47 +0400

    Java_LinkedList

--- src/Prog.java
+++ src/Prog.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Prog {
 
     public static void main(String[] args) {
-        ArrayList<Integer> userAgeList = new ArrayList<>();
+        /*ArrayList<Integer> userAgeList = new ArrayList<>();
         ArrayList<String> userNameList = new ArrayList<>();
 
         userNameList.add("Ivan");
@@ -21,5 +21,38 @@ public class Prog {
         userAgeList.add(50);
 
         System.out.println(userNameList);
+        */
+
+        LinkedList<Integer> userNameList = new LinkedList<>();
+
+        LinkedList<Integer> userAgeLinkedList = new LinkedList<>();
+        userAgeLinkedList.add(null);
+        userAgeLinkedList.add(33);
+        userAgeLinkedList.add(60);
+        System.out.println(userAgeLinkedList);
+        userAgeLinkedList.remove(2);
+        System.out.println(userAgeLinkedList.size());
+        Integer[] userAgeArray = userAgeLinkedList.toArray(new Integer[0]);
+        System.out.println(userAgeLinkedList);
+        System.out.println(userAgeArray[0]);
+
+        System.out.println(userAgeLinkedList.poll());
+        System.out.println(userAgeLinkedList);
+        System.out.println(userAgeLinkedList.peek());
+        userAgeLinkedList.poll();
+        userAgeLinkedList.getLast();
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
